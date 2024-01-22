898a7db0a97016996cd2bf5b5c9062387c101303
littlecm <clittle.norml@gmail.com>
2024-01-22 11:17:06 -0500

    Add files via upload

---
+++ blueprint.js
@@ -0,0 +1,96 @@
+const blueprint = {
+  "name": "Sheet1",
+  "slug": "sheet_1",
+  "fields": [
+    {
+      "key": "customer",
+      "label": "Customer",
+      "type": "string"
+    },
+    {
+      "key": "store",
+      "label": "Store",
+      "type": "string"
+    },
+    {
+      "key": "date_sold",
+      "label": "Date Sold",
+      "type": "string"
+    },
+    {
+      "key": "vehicle_1_status",
+      "label": "Vehicle 1 Status",
+      "type": "string"
+    },
+    {
+      "key": "vehicle_1_year",
+      "label": "Vehicle 1 Year",
+      "type": "number"
+    },
+    {
+      "key": "vehicle_1_make",
+      "label": "Vehicle 1 Make",
+      "type": "string"
+    },
+    {
+      "key": "vehicle_1_model",
+      "label": "Vehicle 1 Model",
+      "type": "string"
+    },
+    {
+      "key": "vehicle_1_stock_number",
+      "label": "Vehicle 1 Stock Number",
+      "type": "string"
+    },
+    {
+      "key": "vehicle_1_vin",
+      "label": "Vehicle 1 VIN",
+      "type": "string"
+    },
+    {
+      "key": "customer_first_name",
+      "label": "Customer First Name",
+      "type": "string"
+    },
+    {
+      "key": "customer_last_name",
+      "label": "Customer Last Name",
+      "type": "string"
+    },
+    {
+      "key": "cell_phone",
+      "label": "Cell Phone",
+      "type": "number"
+    },
+    {
+      "key": "home_phone",
+      "label": "Home Phone",
+      "type": "string"
+    },
+    {
+      "key": "customer_email",
+      "label": "Customer Email",
+      "type": "string"
+    },
+    {
+      "key": "city",
+      "label": "City",
+      "type": "string"
+    },
+    {
+      "key": "state",
+      "label": "State",
+      "type": "string"
+    },
+    {
+      "key": "zip",
+      "label": "Zip",
+      "type": "number"
+    },
+    {
+      "key": "dms_total_gross",
+      "label": "DMS Total Gross",
+      "type": "number"
+    }
+  ]
+}
